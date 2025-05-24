8da5c3d1f3544123b3fd533e7fc0eaef0b971b9a
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2025-05-24 17:18:20 +0530

    ✨ feat: default route /chat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
-import ChatSection from "@/components/ChatSection";
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-
-  return (
-    <ChatSection />
-  );
+  redirect('/chat');
 }
